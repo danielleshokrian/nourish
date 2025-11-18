d23b26e62ea1aaa4c701dfba000bd857edeb6d9a
Danielle <danielle.shokrian@aol.com>
2025-11-18 12:22:29 -0500

    changed vercel link

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,7 +5,7 @@ from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_migrate import Migrate
 from config import config
-
+import re
 
 db = SQLAlchemy()
 ma = Marshmallow()
@@ -21,18 +21,16 @@ def create_app(config_name='default'):
     jwt.init_app(app)
     migrate.init_app(app, db)
 
-    CORS(app, 
-     origins=[
-         "http://localhost:3000",
-         "http://localhost:5173", 
-         "https://nourish-xi.vercel.app",
-         "https://nourish-bctjvzeyo-danielle-shokrians-projects.vercel.app",
-         "https://*.vercel.app"  
-     ],
-     supports_credentials=True,
-     allow_headers=["Content-Type", "Authorization"],
-     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-)
+    CORS(app,
+        origins=[
+            "http://localhost:3000",
+            "http://localhost:5173"
+        ],
+        origin_regex=r"https://.*\.vercel\.app$",  
+        supports_credentials=True,
+        allow_headers=["Content-Type", "Authorization"],
+        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    )
     
     from app.auth import auth_bp
     from app.api import api_bp
